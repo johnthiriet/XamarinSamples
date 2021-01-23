5b02a282b672a0bbc09ad61b3a21f05fceae8a32
John Thiriet <john.thiriet@outlook.com>
2021-01-23 22:15:52 +0100

    Display ads in ads sample.

--- pocpub/pocpub/MainActivity.cs
+++ pocpub/pocpub/MainActivity.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using Android.App;
+using Android.Gms.Ads;
+using Android.Gms.Ads.Initialization;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
+using Android.Widget;
 using AndroidX.AppCompat.App;
-using AndroidX.AppCompat.Widget;
 using Google.Android.Material.FloatingActionButton;
 using Google.Android.Material.Snackbar;
 
@@ -13,17 +15,104 @@ namespace pocpub
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private InterstitialAd _interstitialAd;
+        private AdView _adView;
+
+        private class OnInitializationCompleteListener : Java.Lang.Object, IOnInitializationCompleteListener
+        {
+            private Action<IInitializationStatus> _onInitializationCompleted;
+
+            public OnInitializationCompleteListener(Action<IInitializationStatus> onInitializationCompleted)
+            {
+                _onInitializationCompleted = onInitializationCompleted;
+            }
+
+            public void OnInitializationComplete(IInitializationStatus initializationStatus)
+            {
+                _onInitializationCompleted?.Invoke(initializationStatus);
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                base.Dispose(disposing);
+
+                if (disposing)
+                {
+                    _onInitializationCompleted = null;
+                }
+            }
+        }
+
+        private class MyAdListener : AdListener
+        {
+            private Action _onAdLoaded;
+
+            public MyAdListener(Action onAdLoaded)
+            {
+                _onAdLoaded = onAdLoaded;
+            }
+
+            public override void OnAdLoaded()
+            {
+                base.OnAdLoaded();
+
+                _onAdLoaded?.Invoke();
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                base.Dispose(disposing);
+
+                if (disposing)
+                {
+                    _onAdLoaded = null;
+                }
+            }
+        }
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            var toolbar = FindViewById<AndroidX.AppCompat.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
 
             FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
             fab.Click += FabOnClick;
+
+            MobileAds.Initialize(this, new OnInitializationCompleteListener(OnInitializationComplete));
+        }
+
+        private void OnInitializationComplete(IInitializationStatus initializationStatus)
+        {
+            _adView = new AdView(this);
+
+            _adView.AdSize = AdSize.Banner;
+            _adView.AdUnitId = "ca-app-pub-3940256099942544/6300978111";
+
+            var relativeLayout = FindViewById<RelativeLayout>(Resource.Id.content_relative_layout);
+            relativeLayout?.AddView(_adView);
+
+            AdRequest adRequest = new AdRequest.Builder().Build();
+            _adView.LoadAd(adRequest);
+
+            _interstitialAd = new InterstitialAd(this)
+            {
+                AdUnitId = "ca-app-pub-3940256099942544/1033173712",
+                AdListener = new MyAdListener(OnAdLoaded)
+            };
+
+            _interstitialAd.LoadAd(new AdRequest.Builder().Build());
+        }
+
+        private void OnAdLoaded()
+        {
+            if (_interstitialAd.IsLoaded)
+            {
+                _interstitialAd?.Show();
+            }
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
@@ -55,6 +144,20 @@ namespace pocpub
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+
+            if (disposing)
+            {
+                _adView?.Dispose();
+                _adView = null;
+
+                _interstitialAd?.Dispose();
+                _interstitialAd = null;
+            }
         }
     }
 }
